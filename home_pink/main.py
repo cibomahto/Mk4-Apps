b9e6b8d4d98a539729101eadd4607cad70a80d16
Matt Mets <matt@blinkinlabs.com>
2018-09-02 13:47:47 +0100

    Update homescreen hack

--- home_pink/main.py
+++ home_pink/main.py
@@ -5,10 +5,10 @@ It gets automatically installed when a badge is
 newly activated or reset.
 """
 
-___name___         = "Homescreen (Pink)"
+___name___         = "Homescreen (Bitey)"
 ___license___      = "MIT"
 ___categories___   = ["Homescreens"]
-___dependencies___ = ["homescreen", "shared/logo.png",]
+___dependencies___ = ["homescreen","shared/bitey.png"]
 ___launchable___   = False
 ___bootstrapped___ = True
 
@@ -20,15 +20,7 @@ from tilda import Buttons
 # We ❤️ our sponsors
 init()
 
-# Padding for name
-intro_height = 30
-intro_text = "WASSUP I BE"
-name_height = 60
-status_height = 20
-info_height = 30
-logo_path = "shared/logo.png"
-logo_height = 150
-logo_width = 56
+logo_path = "shared/bitey.png"
 
 # Maximum length of name before downscaling
 max_name = 8
@@ -46,48 +38,11 @@ ugfx.set_default_style(style)
 
 # Logo stuff
 ugfx.display_image(
-    int((ugfx.width() - logo_width) / 2),
-    int((ugfx.height() - logo_height) / 2),
+    0,
+    0,
     logo_path
 )
 
-
-
-# Draw for people to see
-ugfx.orientation(90)
-# Draw introduction
-ugfx.set_default_font(ugfx.FONT_TITLE)
-ugfx.Label(0, ugfx.height() - name_height - intro_height, ugfx.width(), intro_height, intro_text, justification=ugfx.Label.CENTER)
-# Process name
-name_setting = name("Set your name in the settings app")
-if len(name_setting) <= max_name:
-    ugfx.set_default_font(ugfx.FONT_NAME)
-else:
-    ugfx.set_default_font(ugfx.FONT_MEDIUM_BOLD)
-# Draw name
-ugfx.Label(0, ugfx.height() - name_height, ugfx.width(), name_height, name_setting, justification=ugfx.Label.CENTER)
-
-
-
-# Draw for wearer to see
-ugfx.orientation(270)
-# Title
-ugfx.set_default_font(ugfx.FONT_TITLE)
-ugfx.Label(0, ugfx.height() - info_height * 2, ugfx.width(), info_height, "TiLDA Mk4", justification=ugfx.Label.CENTER)
-# info
-ugfx.Label(0, ugfx.height() - info_height, ugfx.width(), info_height, "Long Press MENU", justification=ugfx.Label.CENTER)
-
-ugfx.set_default_font(ugfx.FONT_SMALL)
-status = ugfx.Label(0, ugfx.height() - info_height * 2 - status_height, ugfx.width(), status_height, "", justification=ugfx.Label.CENTER)
-
 # update loop
 while True:
-    text = "";
-    value_wifi_strength = wifi_strength()
-    value_battery = battery()
-    if value_wifi_strength:
-        text += "Wi-Fi: %s%%, " % int(value_wifi_strength)
-    if value_battery:
-        text += "Battery: %s%%" % int(value_battery)
-    status.text(text)
     sleep_or_exit(0.5)
